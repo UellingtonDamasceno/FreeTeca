ef1494589d7b81d046e41573f7c968344f492083
BrendoUrie <brendo.n.s.sousa@gmail.com>
2020-02-28 17:31:06 -0300

    Atributos e setters de Student

--- src/DAO/StudentDAO.java
+++ src/DAO/StudentDAO.java
@@ -5,5 +5,27 @@ package DAO;
  * @author Uellington Conceição
  */
 public class StudentDAO {
+	
+	private int matricula;
+	private String nome;
+	private String email;
+	public int getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
     
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -26,6 +26,7 @@ public class Main extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+    	
         launch(args);
  
     }
