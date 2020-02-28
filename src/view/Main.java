1bddb25b61e3b9bd47933914562a4d0918d2949b
BrendoUrie <brendo.n.s.sousa@gmail.com>
2020-02-28 17:38:33 -0300

    Update Main.java
    Student

--- src/view/Main.java
+++ src/view/Main.java
@@ -26,9 +26,9 @@ public class Main extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+    	
         launch(args);
-        
-        //plugin
+ 
     }
     
 }
