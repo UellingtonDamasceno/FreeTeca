53d60db7c8a2efc55b4b5835c3e883adba1cb7ab
BrendoUrie <brendo.n.s.sousa@gmail.com>
2020-02-29 20:25:51 -0300

    Adicionado ao AdmDAO

---
+++ src/controllers/backend/MainController.java
@@ -0,0 +1,160 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src.controllers.backend;
+
+import controllers.backend.AudioController;
+import facade.FacadeFrontend;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import util.Settings.Scenes;
+import util.Settings.Slider;
+
+/**
+ * FXML Controller class
+ *
+ * @author acmne
+ */
+public class MainController implements Initializable {
+
+    @FXML
+    private ImageView imageViewSlider;
+    @FXML
+    private ToggleButton btnAccessbility;
+    @FXML
+    private Button btnSettings;
+    @FXML
+    private TextField txtEmail;
+    @FXML
+    private PasswordField txtPassword;
+    @FXML
+    private Label lblInfo;
+    @FXML
+    private Label btnRegister;
+    @FXML
+    private Button btnEntry;
+
+    private AudioController audioController;
+
+    @FXML
+    private VBox homePanel;
+    @FXML
+    private HBox hBoxGit;
+    @FXML
+    private Pane paneRoot;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        this.audioController = new AudioController();
+        this.imageViewSlider.setImage(new Image(Slider.FIRST.getImagePath()));
+        this.initialize();
+    }
+
+    private void initialize() {
+        this.txtEmail.textProperty().addListener((observable, oldValue, newValue) -> {
+            this.playAudio(newValue.toCharArray()[newValue.length() - 1] + "");
+        });
+
+//        LinkedList<Image> list = new LinkedList();
+//        list.add(new Image(Slider.values()[0].getImagePath()));
+//        list.add(new Image(Slider.values()[1].getImagePath()));
+//        list.add(new Image(Slider.values()[2].getImagePath()));
+//
+//        while (this.activated) {
+//            for (Image image : list) {
+//                Platform.runLater(() -> {
+//                    this.imageViewSlider.setImage(image);
+//                });
+//
+//                System.out.println("Explodiu");
+//                try {
+//                    Thread.sleep(200);
+//                } catch (InterruptedException ex) {
+//                    Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
+//                }
+//            }
+//            System.out.println("Bomba");
+//
+//        }
+    }
+
+//    private void changeTop() {
+//        ObservableList<Node> childs = this.stackPane.getChildren();
+//
+//        if (childs.size() > 1) {
+//            
+//            Node topNode = childs.get(childs.size() - 1);
+//            
+//            Node newTopNode = childs.get(childs.size() - 2);
+//
+//            topNode.setVisible(false);
+//            topNode.toBack();
+//
+//            newTopNode.setVisible(true);
+//        }
+//    }
+    private void playAudio(String character) {
+        if (this.btnAccessbility.selectedProperty().get()) {
+            audioController.playAudio(character + "");
+        }
+    }
+
+    @FXML
+    private void openSettings(ActionEvent event) {
+    }
+
+    @FXML
+    private void openRegister(MouseEvent event) {
+        this.changeSideBar(Scenes.REGISTER_PERSON);
+    }
+
+    public void changeSideBar(Scenes scene) {
+        try {
+            Parent loadedScreen = FacadeFrontend.getInstance().getScreen(scene);
+            Parent removedScreen = (Parent) this.paneRoot.getChildren().remove(0);
+            FacadeFrontend.getInstance().addScreen(Scenes.HOME_SIDE, removedScreen);
+            this.paneRoot.getChildren().add(loadedScreen);
+        } catch (Exception ex) {
+            Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @FXML
+    private void hide(MouseEvent event) {
+        Platform.runLater(() -> {
+            this.hBoxGit.setVisible(false);
+        });
+    }
+
+    @FXML
+    private void show(MouseEvent event) {
+        Platform.runLater(() -> {
+            this.hBoxGit.setVisible(true);
+        });
+    }
+
+}
---
+++ src/controllers/backend/StageController.java
@@ -0,0 +1,54 @@
+package src.controllers.backend;
+
+import java.util.HashMap;
+import java.util.Map;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import util.Settings.Icons;
+
+/**
+ *
+ * @author Uellington Damasceno
+ */
+public class StageController {
+
+    private Map<String, Stage> stages;
+    private final Pane container;
+
+    public StageController(Stage mainStage) {
+        this.stages = new HashMap();
+        this.container = new Pane();
+        this.stages.put("mainStage", mainStage);
+        mainStage.setResizable(false);
+        mainStage.getIcons().add(new Image(Icons.BOOKS.getIconPath()));
+//        mainStage.initStyle(StageStyle.TRANSPARENT);
+    }
+    
+    public double getStageY(){
+        return this.stages.get("mainStage").getHeight();
+    }
+    public double getStageX(){
+        return this.stages.get("mainStage").getWidth();
+    }
+    
+    public void changeMainStage(String title, Parent content) {
+        this.changeStageContent("mainStage", title, content);
+    }
+
+    public void changeStageContent(String stageName, String title,Parent newContent) {
+        Stage stage;
+        if (this.stages.containsKey(stageName)) {
+            stage = stages.get(stageName);
+        } else {
+            stage = new Stage();
+            stages.put(stageName, stage);
+        }
+        stage.setScene(new Scene(newContent));
+        stage.setTitle(title);
+        stage.show();
+    }
+
+}
